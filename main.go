defa1c58fd27823907556a3276048eec04e0c138
Xianfeng Yuan <xianyuan@gmail.com>
2025-02-26 13:12:04 -0800

    initial api version

---
+++ main.go
@@ -0,0 +1,45 @@
+// ipify-api
+//
+// This is the main package which starts up and runs our REST API service.
+//
+// ipify is a simple API service which returns a user's public IP address (it
+// supports handling both IPv4 and IPv6 addresses).
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/rdegges/ipify-api/api"
+	"github.com/rs/cors"
+)
+
+// main launches our web server which runs indefinitely.
+func main() {
+
+	// Setup all routes.  We only service API requests, so this is basic.
+	router := httprouter.New()
+	router.GET("/", api.GetIP)
+	router.GET("/ip", api.GetIPText)
+
+	// Setup 404 / 405 handlers.
+	router.NotFound = http.HandlerFunc(api.NotFound)
+	router.MethodNotAllowed = http.HandlerFunc(api.MethodNotAllowed)
+
+	// Setup middlewares.  For this we're basically adding:
+	//	- Support for CORS to make JSONP work.
+	handler := cors.Default().Handler(router)
+
+	// Start the server.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Println("Starting HTTP server on port:", port)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+
+}
